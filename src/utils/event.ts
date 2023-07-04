8252917c5b2c1cd22b19d4c36af91c7d47abe70d
Felipe Castro <77942887+Felpo123@users.noreply.github.com>
2023-07-04 04:19:17 +0200

    add Initial event

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,17 +1,27 @@
 let eventGuid = 0;
-let todayStr = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+const todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
 
 export const INITIAL_EVENTS = [
   {
     id: createEventId(),
-    title: 'All-day event',
-    start: todayStr
+    title: "Jessica Alba",
+    start: todayStr,
   },
   {
     id: createEventId(),
-    title: 'Timed event',
-    start: todayStr + 'T12:00:00'
-  }
+    title: "Barbara Broccoli",
+    start: todayStr + "T12:00:00",
+  },
+  {
+    id: createEventId(),
+    title: "Linda Hamilton",
+    start: todayStr + "T24:00:00",
+  },
+  {
+    id: createEventId(),
+    title: "Jennifer Aniston",
+    start: todayStr + "T36:00:00",
+  },
 ];
 
 export function createEventId() {
