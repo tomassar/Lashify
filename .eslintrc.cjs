9249452fc446f579f1e277442f9a83aeba2caba4
Felipe Castro <77942887+Felpo123@users.noreply.github.com>
2023-07-04 04:40:04 +0200

    modify eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,7 @@
 /* eslint-disable */
 const path = require("path");
 
-/** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: path.join(__dirname, "tsconfig.json"),
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
-  },
-  plugins: ["@typescript-eslint"],
   extends: "next",
   rules: {
     "react/no-unescaped-entities": "off",
